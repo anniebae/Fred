5c5b4a0cde864ea5564b0a0e0eeeae2aac82d67b
Annie Bae <bae.annie@gmail.com>
2016-09-21 17:23:46 -0400

    whiskers included

--- js/backbone/views/projectView.js
+++ js/backbone/views/projectView.js
@@ -5,6 +5,7 @@ var ProjectView = Backbone.View.extend ({
 	portfolioTemplate: 	_.template($('#portfolio-template').html()),
 	travelTemplate: 	_.template($("#travel-template").html()),
 	wheretoTemplate: 	_.template($("#whereto-template").html()),
+	whiskersTemplate: 	_.template($("#whiskers-template").html()),
 	initialize: function() {
 		this.renderNav();
 		this.showPortfolio();
@@ -12,7 +13,8 @@ var ProjectView = Backbone.View.extend ({
 	events: {
 		'click #btn-portfolio' 	: 'showPortfolio',
 		'click #btn-travel' 	: 'showTravel',
-		'click #btn-whereto'	: 'showWhereto'
+		'click #btn-whereto'	: 'showWhereto',
+		'click #btn-whiskers'	: 'showWhiskers'
 	},
 	renderNav: function() {
 		this.$el.html(this.navTemplate());
@@ -35,6 +37,12 @@ var ProjectView = Backbone.View.extend ({
 	showWhereto: function() {
 		this.renderNav();
 		$('#project-carousel').html(this.wheretoTemplate());
+		owlCarouselTrigger();
+	},
+
+	showWhiskers: function() {
+		this.renderNav(); 
+		$('#project-carousel').html(this.whiskersTemplate());
 		owlCarouselTrigger();
 	}
 
